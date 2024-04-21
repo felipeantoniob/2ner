012a2262fb8e4a81d77fa1935aaf1687b233797d
felipeantoniob <73531907+felipeantoniob@users.noreply.github.com>
2024-04-21 21:20:38 +0800

    Add tuner to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import usePitch from "./hooks/usePitch";
 import useInterval from "./hooks/useInterval";
+import frequencyToNote from "./utils/frequencyToNote";
 
 function App() {
   const {
@@ -9,7 +10,10 @@ function App() {
     stopPitchDetection,
     pitch,
     clarity,
+    decibels,
   } = usePitch();
+
+  const note = frequencyToNote(pitch);
 
   useInterval(async () => {
     await updatePitch();
@@ -17,10 +21,34 @@ function App() {
 
   return (
     <>
-      <button onClick={initPitchDetection}>Init pitch detection</button>
-      <button onClick={stopPitchDetection}>Stop pitch detection</button>
+      <button onClick={initPitchDetection}>Start</button>
+      <button onClick={stopPitchDetection}>Stop</button>
       <div>Pitch is {pitch} Hz</div>
       <div>Clarity is {clarity}%</div>
+      <div>{decibels.toFixed(2)}dB</div>
+      <div
+        style={{
+          width: 50,
+          height: 50,
+          margin: "0 auto",
+          borderRadius: 100,
+          backgroundColor:
+            clarity < 85
+              ? "#787878"
+              : Math.abs(note.cents) > 10
+                ? "red"
+                : "green",
+        }}
+      />
+      {clarity > 85 && (
+        <>
+          <p style={{ fontSize: 32 }}>
+            {pitch && note.name}
+            {pitch && note.octave}
+          </p>
+          <div>{pitch && note.cents} cents</div>
+        </>
+      )}
     </>
   );
 }
